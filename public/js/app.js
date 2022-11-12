8bb6f7376d6488c33481dd5a2f0e547ac225a3d4
LucasGoni <lucas.rgoni@gmail.com>
2022-11-12 16:07:22 -0300

    feat: subtract method

--- public/js/app.js
+++ public/js/app.js
@@ -6,5 +6,21 @@ console.log(add, subtract);
 add.addEventListener("click", () => {
   let output = document.querySelector("#output");
   let result = Number(output.innerText) + 1;
+
+  /* if (result > 10) {
+    result = 0;
+  }
+*/
+  output.innerText = result;
+});
+
+subtract.addEventListener("click", () => {
+  let output = document.querySelector("#output");
+  let result = Number(output.innerText) - 1;
+
+  if (result < 0) {
+    result = 0;
+  }
+
   output.innerText = result;
 });
